44c63e29e3ea7f6980156f3e5374993c87aba79e
Kersti <you@example.com>
2019-06-15 08:15:43 -0600

    4.3 Assign variable a = 7, and b ==a

---
+++ 5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,10 @@
+// Learned to assign a variable to a number values
+//and learned we can assign one variable to another!
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+var a = 7;
+var b = a;
