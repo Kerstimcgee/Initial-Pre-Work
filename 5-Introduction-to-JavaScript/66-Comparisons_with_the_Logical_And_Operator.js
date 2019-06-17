8c67f5abf3dbf225c8f59dcc51c7613f11b22ba4
Kersti <you@example.com>
2019-06-17 11:43:13 -0600

    5.66 setup conditions for numbers 50, 25, and less than

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,16 @@
+//learn how to use &&t to add on conditional
+//statements in if condition
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+      return "Yes";
+  }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
