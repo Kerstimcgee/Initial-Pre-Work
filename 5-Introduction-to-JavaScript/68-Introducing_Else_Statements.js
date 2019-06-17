e0dbcff51eb6999fdbd812f58a2ca18b53a638c6
Kersti <you@example.com>
2019-06-17 12:21:30 -0600

    5.68 reduce code by using else statement

---
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -0,0 +1,17 @@
+//learn to use an if/else rather than multiple ifs into one conditionalfunction testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5 ) {
+    result = "Bigger than 5";
+  } else {
+    return "5 or Smaller"
+  }
+
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
