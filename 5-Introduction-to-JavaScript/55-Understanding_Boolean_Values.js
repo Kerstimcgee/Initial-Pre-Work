fa14e2f406ce008c4aa1a4751844a51fee5ababb
Kersti <you@example.com>
2019-06-17 11:03:01 -0600

    5.55 Gave welcomeToBooleans a val

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+//learn to assign our first boolean, t/f value to a var.
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
