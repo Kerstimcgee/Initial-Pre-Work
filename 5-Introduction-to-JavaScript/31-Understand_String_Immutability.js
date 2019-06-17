c6d03e2caf34a47a7ebd70fca6df815dc84ea790
Kersti <you@example.com>
2019-06-17 08:28:57 -0600

    5.31 Edit first char in myStr

---
+++ 5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,8 @@
+// You cannot alter a string with indexing/we learned string values
+//aren't immutable
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"// Fix Me
