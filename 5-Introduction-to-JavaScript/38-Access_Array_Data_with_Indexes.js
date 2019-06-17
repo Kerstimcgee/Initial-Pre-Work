5a7ef8e5f7d2a34aa1b17c2c42f0c61e1fa237d5
Kersti <you@example.com>
2019-06-17 09:17:47 -0600

    5.38 take first data piece from array for our sample

---
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,11 @@
+//here, we learned to extract a data piece from an array (cool!)
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
