d3d2ee57df721277a637586ddc365c38cf994155
Kersti <you@example.com>
2019-06-17 09:14:38 -0600

    5.37 create nested array of name/age

---
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,7 @@
+//We learned to nest an array within an array
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Fido", 3], ["Spot", 7]];
