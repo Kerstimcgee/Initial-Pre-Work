172b2cbdb1af5d52d7c1c920714b917e29cb66d2
Kersti <you@example.com>
2019-06-17 11:14:19 -0600

    5.58 make sure "true" data is always a numerical 7

---
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -0,0 +1,13 @@
+//Learn to apply strict val operator === in order to evaluate between
+//multi data types
+
+// Setup
+function testStrict(val) {
+  if (val===7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(10);
