532ed9caecefdd81be542af9d3c72220e2f5f6c4
Kersti <you@example.com>
2019-06-17 01:26:01 -0600

    5.25 Create var myStr, two strings together

---
+++ 5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+//Concatination- we learned how to join two strings
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start." + " This is the end.";
