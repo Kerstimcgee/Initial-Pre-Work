9bf50c9f31b1d43f4bbd7e558ff1724c9e8dc26c
Kersti <you@example.com>
2019-06-17 11:11:02 -0600

    5.57 set testEqual true at val of 12

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,13 @@
+//we learned to use the "equivalent" equals sign to set up our true
+//or false conditional
+
+// Setup
+function testEqual(val) {
+  if (val==12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
