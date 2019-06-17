bfcda9167ce8d183015dee52696209d425539873
Kersti <you@example.com>
2019-06-17 11:22:34 -0600

    5.61 determine if the val does not equal 17

---
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -0,0 +1,16 @@
+//using the strict non-operator for not eqivalent data types
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val!==17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
