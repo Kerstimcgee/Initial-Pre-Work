5b1bbe403f4923affc81b8dd755b1323e43d9127
Kersti <you@example.com>
2019-06-16 23:22:27 -0600

    5.6 Edit all var names to camel case for js use

---
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,10 @@
+// we learned how to apply camel case to our variables for js
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
