3c14a893b843fd0e0a48d4a3907bfaf5f7fa9e31
Kersti <you@example.com>
2019-06-17 09:09:33 -0600

    5.36 combine letters and alpha to array

---
+++ 5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,7 @@
+//Within an array now, we combine two data type values (num and alpha)
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["this chord uses the flat ", 7];
