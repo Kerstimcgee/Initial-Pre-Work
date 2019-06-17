25f850b5260e4d63818448413c1bbeb37a397340
Kersti <you@example.com>
2019-06-17 08:37:52 -0600

    5.34 Grab "a" char from latter end of last name into a var

---
+++ 5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+//Use index from 0 to get another char right to left, last to first.
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
