ca275b7a358d11e22a8048e3bbe0c367c8f8b347
Kersti <you@example.com>
2019-06-17 08:34:51 -0600

    5.33 create var finding last char of last name

---
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+//Learn to use .lenght and index [] to find last letter of string
+
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
