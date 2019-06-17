beee5d244df15b068c4a6bd18ed50af72af1e75b
Kersti <you@example.com>
2019-06-17 09:48:10 -0600

    5.44 remove first entry from array

---
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -0,0 +1,14 @@
+//learn the shift func. similar to pop- shift removes one val,
+//this time from the begin of an array, not the end.
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35])
+
+// Only change code below this line.
