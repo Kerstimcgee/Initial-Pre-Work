0a3ac1397c6812b185e84c61b119fd97011ba00a
Kersti <you@example.com>
2019-06-17 10:40:03 -0600

    5.52 write working func addFive without return

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,16 @@
+//write a function that adds five but doesn't use a return statement
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+var sum = 0;
+function addFive() {
+  sum = sum + 5;
+}
+
+
+// Only change code above this line
+var returnedValue = addFive();
