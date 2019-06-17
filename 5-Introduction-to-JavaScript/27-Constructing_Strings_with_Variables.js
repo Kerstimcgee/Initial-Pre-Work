3746ac71c987507e03d6266d9c8ad78e36da058a
Kersti <you@example.com>
2019-06-17 01:36:21 -0600

    5.27 Merge together three name strings

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,10 @@
+//create a string using a multiple variables. Here we learn to merge our
+//name with a couple of sayings
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Kersti";
+var myStr = "Hello, my name is " + myName + " and I am well!";
