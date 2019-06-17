fbaa7922300e720e33aa776dc11dc515d1baa239
Kersti <you@example.com>
2019-06-17 12:57:43 -0600

    5.70 modified logic in if elif else

---
+++ 5-Introduction-to-JavaScript/70-Logical_Order_in_If_Else_Statements.js
@@ -0,0 +1,12 @@
+//learn to switch conditions so that logical walkthrough gives
+//correct answers for each statement case
+
+function orderMyLogic(val){
+  if (val < 5) {
+    return "Less than 5";
+  }   else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10"
+  }
+}
