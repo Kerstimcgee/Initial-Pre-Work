bc46dd0ddc8b596b82af122cca62a21a1bdb8b70
Kersti <you@example.com>
2019-06-17 00:38:00 -0600

    5.18 Modify variable assignments to use -= operator

---
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -0,0 +1,12 @@
+//apply and learn of the -= operator, similarly and apply to variable
+//assignments
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
