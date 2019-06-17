4b8375e4c90f68da82c844ea590eede699ac4231
Kersti <you@example.com>
2019-06-17 11:17:45 -0600

    5.59 set func to determine equal data types

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,12 @@
+//learn to check for exact data type match by switching to using ===symbol
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
