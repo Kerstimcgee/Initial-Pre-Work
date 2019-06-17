86fd8218ff707e4adffb8d024a5fcb2efce8e7ed
Kersti <you@example.com>
2019-06-17 11:31:21 -0600

    5.63 created return string for the condition 20 or over and 10 or over

---
+++ 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -0,0 +1,16 @@
+//comparison operator - learned to use a > OR = statement
+
+function testGreaterOrEqual(val) {
+  if (val>=20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val>=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
