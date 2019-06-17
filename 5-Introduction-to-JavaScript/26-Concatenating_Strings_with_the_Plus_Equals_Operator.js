f8ec920c6add6314c5fb021edc4f2f5f9063dbd6
Kersti <you@example.com>
2019-06-17 01:31:31 -0600

    5.26 Merge two strings, utilize + - operator method

---
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -0,0 +1,10 @@
+//Concat two strings using += on a variable to merge two.
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
