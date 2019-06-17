14fc0a5834ae2179385ebb1c7335033b7c607f56
Kersti <you@example.com>
2019-06-17 11:59:57 -0600

    5.67 assign "inside" and "outside" conditionals

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,16 @@
+//Learned to run "inside" or "outside" strings under one line or conditional statement ||
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val <10 || val > 20) {
+    return "Outside";
+  }
+
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
