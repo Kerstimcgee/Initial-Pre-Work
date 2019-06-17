d486bc1d8786f1e6ce73828c2fb6f4838a416ca5
Kersti <you@example.com>
2019-06-17 10:20:08 -0600

    5.51 finish timesFive function to take in num

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,12 @@
+//learned to pass in argument "num" for multiplication functionWithArgs// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+
+
+console.log(minusSeven(10));
