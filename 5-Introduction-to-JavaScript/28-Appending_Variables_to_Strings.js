84fbad13c95c6a37bc5968b57453e2026a43227c
Kersti <you@example.com>
2019-06-17 01:48:21 -0600

    5.28 merge string variables, utililized += operator

---
+++ 5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,13 @@
+//we learned to add strings together with variables using our
+//+= operator.
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective= "interesting";
+var myStr = "Learning to code is ";
+myStr += someAdjective;
