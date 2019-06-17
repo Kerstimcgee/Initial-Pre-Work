1c1407e88392bdf94b72cc6ab6b7df6173975e29
Kersti <you@example.com>
2019-06-17 10:04:03 -0600

    5.48 define myGlobal outside of function

---
+++ 5-Introduction-to-JavaScript/48-Global_Scope_and_Functions.js
@@ -0,0 +1,20 @@
+// We learned to assign a global variable outside of function.
+// Declare your variable here
+var myGlobal = 10
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
