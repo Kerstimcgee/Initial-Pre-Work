353d7cfbb2ef439c285ac6fc4bbb1854e9fe0656
Kersti <you@example.com>
2019-06-17 10:16:00 -0600

    5.50 over write outerwear selection to local var "sweater"

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,15 @@
+//manipulate local var to overwrite global, despite same name!
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
