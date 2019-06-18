d4483e2663d86bf2c1fa256fcac3bcf81efeb916
Kersti <you@example.com>
2019-06-18 08:56:06 -0600

    5.78 ad undefined statement for numbers less than zero

--- 5-Introduction-to-JavaScript/78-Return_Early_Pattern_for_Functions.js
+++ 5-Introduction-to-JavaScript/78-Return_Early_Pattern_for_Functions.js
@@ -1,8 +1,17 @@
-// learn to go back to use conditions to eliminate elif else, if in our scenario
+//learn to return undefined in order to stop running program with wrong parameters
 
-function isLess(a, b) {
-  return a <= b;
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if (a < 0  || b < 0) {
+    return undefined ;}
+
+
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-// Change these values to test
-isLess(10, 15);
+// Change values below to test your code
+abTest(2,2);
