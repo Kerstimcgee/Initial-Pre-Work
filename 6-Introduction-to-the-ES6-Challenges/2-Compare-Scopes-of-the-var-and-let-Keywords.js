e35eaee274ebde8f7baf32d40688c600b6915db9
Kersti <you@example.com>
2019-06-18 15:15:57 -0600

    6.2 declare i variable using let

---
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -0,0 +1,13 @@
+//leaned to declare our variables with let to be used locally
+
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+return i
+}
