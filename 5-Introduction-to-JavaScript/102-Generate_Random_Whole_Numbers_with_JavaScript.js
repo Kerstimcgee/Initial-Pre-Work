1d9651524aefb733111ad33addffea831f2534a8
Kersti <you@example.com>
2019-06-18 14:19:46 -0600

    5.102 generate random whole num for simulation

---
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -0,0 +1,12 @@
+//we learned we can use floor to bring a number to nearest whole number and
+//then how to generate a random decimal number, then convert int
+// to it's whole (*20) 
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
