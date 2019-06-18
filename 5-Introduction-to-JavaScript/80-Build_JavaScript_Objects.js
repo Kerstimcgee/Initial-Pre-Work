72c984044c7fb6d17f6a59084b2924dfeb58ed87
Kersti <you@example.com>
2019-06-18 09:48:52 -0600

    5.80 create my dog

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,21 @@
+//Learn to create a javascript object...dog-ject. 
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Liesel von Trapp",
+  "legs": 3,
+  "tails": 1,
+  "friends": ["Anyone who can pet me"]
+
+
+
+};
