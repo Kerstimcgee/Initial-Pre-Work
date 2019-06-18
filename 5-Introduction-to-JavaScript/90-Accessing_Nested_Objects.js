5eb80e3a8cdb805d58da9626b973e63a024a1baa
Kersti <you@example.com>
2019-06-18 12:44:27 -0600

    5.90 access contents of glovebox

---
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -0,0 +1,16 @@
+//learn to access content of glovebox by trying dot and bracket notation
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
