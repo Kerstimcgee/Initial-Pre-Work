7c6b5d14af4da4704976cdee3d76ce4c86d6f280
Kersti <you@example.com>
2019-06-18 10:33:07 -0600

    5.81 extract shrit and hat data from our object!

---
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -0,0 +1,15 @@
+//learn a new way to extract data. This time using js objects
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
