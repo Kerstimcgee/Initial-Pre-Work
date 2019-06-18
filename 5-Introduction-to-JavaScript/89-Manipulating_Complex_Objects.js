94e1879536a82a0e3a99fadd63b7634c94bc6ef6
Kersti <you@example.com>
2019-06-18 12:32:19 -0600

    5.89 add u2 album into data

---
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,30 @@
+//Add in new album...learn to add json array to our data section.
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true,
+  },
+  // Add record here,
+  {
+    "artist": "U2",
+    "title": "Joshua Tree",
+    "release_year": 1987,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+
+
+
+
+  }
+];
