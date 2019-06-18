d1248089f4f80c14ddee2a221ad53ecb1c4822e6
Kersti <you@example.com>
2019-06-18 14:00:50 -0600

    5.101 produce random decimal number

---
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,14 @@
+//learned to generate random dec. number with Math.random function,
+//but set it so that it generates one only if larger than 0 
+
+function randomFraction() {
+
+  // Only change code below this line.
+    var result = 0;
+    while (result === 0) {
+    result = Math.random();
+  }
+return result;
+
+  // Only change code above this line.
+}
