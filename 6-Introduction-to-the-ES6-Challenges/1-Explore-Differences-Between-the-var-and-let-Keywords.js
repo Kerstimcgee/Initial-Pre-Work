b53010e51c5cb44540dbd32661f8e034eb8450a7
Kersti <you@example.com>
2019-06-18 15:10:00 -0600

    6.1 implement let for catname and quote

---
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -0,0 +1,12 @@
+//using let to make variables harder to rename mistakenly
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
