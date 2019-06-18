c2c3cc3d69c78374a0c8d875d22e72e04ffdf542
Kersti <you@example.com>
2019-06-18 16:57:28 -0600

    6.10 edit code to take variable as arg. Implement rest op. "..."

---
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,10 @@
+//Here we learn to take in a number of arguements that can vary
+//using the ... function
+
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
