8100ee59f1d9313a133e5b3d0a9e7574e5bde6f6
Kersti <you@example.com>
2019-06-18 11:03:51 -0600

    5.85 update bark property

---
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -0,0 +1,23 @@
+//learn to update more data in our object
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "bark"
+
+// Only change code below this line.
