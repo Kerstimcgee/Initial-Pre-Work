254e8ffb3f332f36f9420399b676ae2dbd974a30
Kersti <you@example.com>
2019-06-18 10:55:12 -0600

    5.83 get player number

---
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -0,0 +1,14 @@
+//learned to get our player number out of the data and assign to new var.
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+// Setup
+
+var playerNumber = 16;
+var player = testObj[playerNumber];
