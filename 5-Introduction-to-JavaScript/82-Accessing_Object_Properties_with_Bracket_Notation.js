3c99ce91213ed9d856e398fb8b8dd3fc6cd6a6b2
Kersti <you@example.com>
2019-06-18 10:39:37 -0600

    5.82 assign drink and entree variables

---
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -0,0 +1,13 @@
+//assign extracted data to a variable. This is getting cool!
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue= testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];  // Change this line
