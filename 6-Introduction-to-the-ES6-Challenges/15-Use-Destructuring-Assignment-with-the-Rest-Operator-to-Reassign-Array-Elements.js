979092a361fc60944bb50770ad66d19c9f79bb52
Kersti <you@example.com>
2019-06-18 19:08:55 -0600

    6.15 Get last items of array "source" into a new arr that removes a and b or position 0 and 1

---
+++ 6-Introduction-to-the-ES6-Challenges/15-Use-Destructuring-Assignment-with-the-Rest-Operator-to-Reassign-Array-Elements.js
@@ -0,0 +1,14 @@
+//learned to change an array to a const in order to remove beginning
+//of array and create a new array with remainder of the items.
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  const [a,b,...arr] = list; // change this
+  // change code below this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
