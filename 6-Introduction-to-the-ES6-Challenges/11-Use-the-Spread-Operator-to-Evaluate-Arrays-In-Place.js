a5d3fdccf6deef40d963b8316f12519866cb9733
Kersti <you@example.com>
2019-06-18 18:26:04 -0600

    6.11 spread array 1 to array 2

---
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -0,0 +1,8 @@
+//use ...arr to put first array into second new array, or spread the array
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
