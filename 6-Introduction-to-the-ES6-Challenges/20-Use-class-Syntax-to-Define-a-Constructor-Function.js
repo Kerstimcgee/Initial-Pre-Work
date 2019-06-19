ee341184f1272acaaf40a602e1400a3da81ff9ac
Kersti <you@example.com>
2019-06-18 19:54:30 -0600

    6.20 Create new vegetable function to emit "carrot"

---
+++ 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
@@ -0,0 +1,15 @@
+//learned to use class and construct syntax to make a new object
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+  class Vegetable {
+    constructor(name){
+      this.name = name;
+    }
+  }
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
